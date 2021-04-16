678c23d41e1b6d0ee43ee1ab890efb34fe8d5c17
wasgton <wasgton.it@gmail.com>
2021-04-16 16:30:47 -0300

    Estilizando respostas e configurando os estados

--- lib/pages/trivia_screen/trivia_screen.dart
+++ lib/pages/trivia_screen/trivia_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TriviaScreen extends StatefulWidget {
@@ -6,64 +7,128 @@ class TriviaScreen extends StatefulWidget {
 }
 
 class _TriviaScreenState extends State<TriviaScreen> {
+
+  int answer = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: Image.asset(
             'lib/assets/images/logo_branca.png'
-            ,height:48
+            ,height:27
           ),
           backgroundColor: Color(0xff374952),
           centerTitle:true,
       ),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(),
-          ]
-        ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          // color: Colors.red,
-            boxShadow:[
-              BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 1,
-                  blurRadius: 7,
-                  offset: Offset(0, 1)
-              )
-            ]
-        ),
-        padding: EdgeInsets.fromLTRB(0, 24, 0, 24),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FlatButton(
-              onPressed: (){
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context)=> TriviaScreen())
-                );
-              },
-              child: Text(
-                'Responder'
-                ,style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.w600
-              ),
-              ),
-              color: Color(0xffDA0175),
-              textColor: Color(0xffF7F7F7),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.fromLTRB(60, 11, 60, 11),
+      body: Column(
+        children:[
+          Container(
+            margin: EdgeInsets.fromLTRB(32, 56, 32, 10),
+            // color: Colors.black,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'PERGUNTA 1',
+                  style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                Text(
+                  'Lorem ipsum dolor sit?',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          ),
+          Expanded(
+              child: ListView(
+                children: [
+                  Container(
+                    margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.30),
+                          spreadRadius: 2,
+                          blurRadius:3,
+                          offset: Offset(1,3)
+                        ),
+                      ]
+                    ),
+                    child: RadioListTile(
+                      groupValue: answer,
+                      onChanged: (value) {
+                        setState(() {
+                          answer = value;
+                        });
+                      },
+                      value: 1,
+                      title: Text('Teste 1'),
+                    )
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.30),
+                          spreadRadius: 2,
+                          blurRadius:3,
+                          offset: Offset(1,3)
+                        ),
+                      ]
+                    ),
+                    child: RadioListTile(
+                      groupValue: answer,
+                      onChanged: (value) {
+                        setState(() {
+                          answer = value;
+                        });
+                      },
+                      value: 2,
+                      title: Text('Teste 2'),
+                    )
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(32, 10, 32, 10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.30),
+                          spreadRadius: 2,
+                          blurRadius:3,
+                          offset: Offset(1,3)
+                        ),
+                      ]
+                    ),
+                    child: RadioListTile(
+                      groupValue: answer,
+                      onChanged: (value) {
+                        setState(() {
+                          answer = value;
+                        });
+                      },
+                      value: 3,
+                      title: Text('Teste 3'),
+                    )
+                  ),
+                ],
+              )
+          )
+        ],
       ),
     );
   }
